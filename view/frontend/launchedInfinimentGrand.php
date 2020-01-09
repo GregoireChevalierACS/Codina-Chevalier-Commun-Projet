de632e0f40c2a7fe91c083dad155fe3811b89b05
GregoireChevalierACS <aagchevalier@gmail.com>
2020-01-09 13:24:15 +0000

    Commit concernant les updates relatives à la vue infiniment grand  : Visuels, model et controller, et le css.

--- view/frontend/launchedInfinimentGrand.php
+++ view/frontend/launchedInfinimentGrand.php
@@ -4,14 +4,19 @@ $style = 'public/css/launchedInfinimentGrand.css';
  ?>
  
 <?php ob_start(); ?>
-
+<p class="boutonRetour">
 <a href="index.php?action=launch">De retour chez nous</a>
- <div class ="d-flex">
+</p>
+ <div class ="d-flex parallax">
  <?php foreach($namesPhenomenon as $name){ ?>
       <?php $nom = $name['nameOfTopic'];
             $concatenation = str_replace(' ', '', $nom);
       ?>
-      <a href="index.php?action=phenomeneIG&name=<?=$name['nameOfTopic']?>"><img src="<?=$name['visuel'] ?>" class="boutonIG" id="<?= $concatenation ?>"></a>
+      <p class="type"><?=$name['nameOfTopic']?>
+            <a href="index.php?action=phenomeneIG&name=<?=$name['nameOfTopic']?>">
+            <img src="<?=$name['visuel'] ?>" class="boutonIG" id="<?= $concatenation ?>">
+            </a>
+      </p>
  <?php } ?> 
 </div> 
 <?php $content = ob_get_clean(); ?>
