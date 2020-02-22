9a6f50d3465e310f9c1084cae3eb478e6959d15b
Aranxa.C <tassa780@gmail.com>
2020-02-22 14:20:07 +0000

    Remise au propre de l'interface admin, avecl'ajout du aside concernant les topics

--- view/admin/interfaceAdmin.php
+++ view/admin/interfaceAdmin.php
@@ -1,7 +1,7 @@
 <?php 
 $title = "Admin";
 $style = "public/css/identification.css"; 
-$script = "";
+$script = "public/js/interfaceAdmin.js";
 
 function changeKeyToValue($key){
 
@@ -20,13 +20,13 @@ function changeKeyToValue($key){
 ob_start(); ?>
 
 <?php require('view/admin/navbar.php'); 
-if($suppression){ ?> 
+if($suppression){ ?>
 
 <div class="container">
-<div class="alert alert-success alert-dismissible" > 
-<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-Le sujet a bien été supprimé ! 
-</div>
+    <div class="alert alert-success alert-dismissible">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+        Le sujet a bien été supprimé !
+    </div>
 </div>
 
 <?php
@@ -35,38 +35,53 @@ $suppression = false;
 ?>
 
 
-<div class="container-fluid mt-5">
+<div class="container-fluid mb-3">
+
+
+
+    <?php 
+foreach($themes as $key => $themeBoucle){ 
+    $valueTheme = changeKeytoValue($key);
+                ?>
     <div class="row">
 
-        <?php 
-foreach($themes as $key => $themeBoucle){ 
-    $valueTheme = changeKeytoValue($key)?>
-    
-        <div class="col-md-4 col-xs-12 col-sm-12 ">
-            <div class="d-flex flex-wrap">
-              <div>  <h2 class="text-break"><?= $valueTheme ?></h2> </div>
-               <div> <a class="btn btn-success ml-2" href="admin.php?action=ajout&name=<?=$key?>"> Ajouter un sujet</a> </div>
-            </div>
-            <a class="btn btn-outline-secondary" href="#<?=$key?>" data-toggle="collapse">Plus de détails</a>
-            <div id="<?=$key?>" class="collapse"> <?php 
-foreach ($themeBoucle as $sujet){ ?>
-                <div>
-                    <h3><?= $sujet['topicName'] ?></h3>
-                    <a href="admin.php?action=modif&id=<?= $sujet['id']?>&table=<?=$key?>"><button type="button"
-                            class="btn btn-primary">Modifier</button></a>
-                    <a href="admin.php?action=confirmation&id=<?= $sujet['id']?>&table=<?=$key?>&nameTopic=<?=$sujet['topicName']?>"><button type="button"
-                            class="btn btn-danger">Supprimer</button></a>
-                </div>
 
-                <?php } ?>
+        <div class="col-md-2 col-xs-12 col-sm-12 aside">
+            <div id="<?=$valueTheme?>">
+                <h2 class="text-wrap topic"><?= $valueTheme ?></h2>
             </div>
         </div>
-        <?php } ?>
+       
 
 
+        <div class="col-md-10 subject" id=<?=$key?>>
+         
+        <h3 class="text-center"><?=$valueTheme?></h3>
+        <div> <a class="btn btn-success mb-2" href="admin.php?action=ajout&name=<?=$key?>"> Ajouter un sujet</a> </div>
+            <?php       
+foreach ($themeBoucle as $sujet){?>
+
+            
+            <h3><?= $sujet['topicName'] ?></h3>
+            <a href="admin.php?action=modif&id=<?= $sujet['id']?>&table=<?=$key?>"><button type="button"
+                    class="btn btn-primary">Modifier</button></a>
+            <a
+                href="admin.php?action=confirmation&id=<?= $sujet['id']?>&table=<?=$key?>&nameTopic=<?=$sujet['topicName']?>"><button
+                    type="button" class="btn btn-danger">Supprimer</button></a>
+
+
+
+            <?php } ?>
+        </div>
     </div>
+    <?php } ?>
+
+</div>
+
+
+</div>
 
 
 
-    <?php $content = ob_get_clean(); ?>
-    <?php require('view/frontend/template.php'); ?>
+<?php $content = ob_get_clean(); ?>
+<?php require('view/frontend/template.php'); ?>
